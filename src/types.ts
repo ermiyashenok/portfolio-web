85902508bb9cae511fa1c1fb636c27bbaa7df78d
ermiyashenok <ermiyashenok44@gmail.com>
2026-06-25 13:26:57 +0300

    Update dependencies and extend Project type definitions

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,15 @@
 export interface Project {
   id: string;
+  slug: string;
   title: string;
   description: string;
+  inDepthDescription: string;
+  learnings: { category: string; points: string[] }[];
   tags: string[];
   link?: string;
   github?: string;
   images?: string[];
+  mobileOnlyPreview?: boolean;
 }
 
 export interface Experience {
